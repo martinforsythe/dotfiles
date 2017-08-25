e83352a7cba99a024235c98fd18b31b6d9b2f92f
mbloodforsythe <martin.zwirnerforsythe@gmail.com>
2017-08-25 11:06:44 -0400

    add note for future atom packages work

--- atom_packages.sh
+++ atom_packages.sh
@@ -1,5 +1,8 @@
 # To backup atom packages
 apm list --installed --bare > atom_packages.list
 
+# To backup atom settings:
+# need only the .json, .cson, .coffee and .less files in ~/.atom
+
 # To reinstall atom packages
 #apm install `cat atom_packages.list`
