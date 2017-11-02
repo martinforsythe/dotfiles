1f8d9737f0202a02ab76d19c8f31f3bec74c8e60
mbloodforsythe <martin.zwirnerforsythe@gmail.com>
2017-11-02 09:44:44 -0400

    starting to flesh out initial setup script

--- run_first.sh
+++ run_first.sh
@@ -28,4 +28,26 @@ cd $DOTFILES
 brew tap Homebrew/bundle
 brew bundle
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# command line tools
+xcode-select --install
+
+# casks
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install atom
+brew cask install gimp
+brew cask install inkscape
+brew cask install xquartz
+brew cask install slack
+brew cask install gitup
+brew cask install mactex
+brew cask install adobe-acrobat-reader
+brew cask install dropbox
+
+# Install Inconsolata font
+echo "Installing Inconsolata font"
+curl https://github.com/google/fonts/blob/master/ofl/inconsolata/Inconsolata-Regular.ttf --output /Library/Fonts/Inconsolata-Regular.ttf
+curl https://github.com/google/fonts/blob/master/ofl/inconsolata/Inconsolata-Bold.ttf --output /Library/Fonts/Inconsolata-Bold.ttf
+
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
