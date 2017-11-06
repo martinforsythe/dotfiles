614ee486cabc85e7b6124849cff90f2a948ad999
mbloodforsythe <martin.zwirnerforsythe@gmail.com>
2017-11-05 19:51:14 -0500

    brew: configuration of packages that I sometimes use
    since brew bundle does not support config options
    this is a script that specifies preferred isntall
    options for some packages that I ocassionally use
    but do not necessarily want to demand be installed
    in the run_first.sh

---
+++ brew_not_installed.sh
@@ -0,0 +1,9 @@
+brew install ghostscript --with-x11
+brew install fftw --with-fortran
+brew install imagemagick --with-opencl --with-ghostscript --with-fontconfig --with-fftw --with-pango
+brew install graphicsmagick --with-ghostscript --with-x11
+brew install node --with-openssl
+brew install llvm --with-python --with-graphviz
+brew install boost --c++11 --with-icu4c
+brew install boost-python --c++11 --with-python3
+brew install eigen
