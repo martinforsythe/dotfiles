29a241ce73b8ff850f5723b070dfcc51b578df10
mbloodforsythe <martin.zwirnerforsythe@gmail.com>
2018-03-23 14:24:14 -0400

    Fixup: use symlinks for zsh and fish installs

--- install_fish.sh
+++ install_fish.sh
@@ -4,8 +4,9 @@ curl -L https://get.oh-my.fish | fish
 # install some fish shell plugins
 fish ./fish/install_omf_plugins.fsh
 
-# copy fish configuration files
+dotfiles=$HOME/dotfiles
+# symbolic links to fish configuration files
 mkdir -p $HOME/.config/omf
 mkdir -p $HOME/.config/fish
-yes | cp -rf ./fish/before.init.fish $HOME/.config/omf/before.init.fish
-yes | cp -rf ./fish/config.fish $HOME/.config/fish/config.fish
+ln -s $dotfiles/fish/before.init.fish $HOME/.config/omf/before.init.fish
+ln -s $dotfiles/fish/config.fish $HOME/.config/fish/config.fish
