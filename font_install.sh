6ee731d09de6c9b992100af673559e65dd9c41f6
mbloodforsythe <martin.zwirnerforsythe@gmail.com>
2018-03-23 13:30:28 -0400

    fonts: Add nerd-font version of Inconsolata
    oh-my-zsh and oh-my-fish themes use icons in the modified versions of this font.

--- font_install.sh
+++ font_install.sh
@@ -1,7 +1,11 @@
 # Install Inconsolata font which is used by iterm and Terminal configurations
 installed_fonts=$(ls /Library/Fonts/)
-if not_contains "$installed_fonts" "Inconsolata"; then
+# Check if Inconsolata is already installed
+if [[ ${installed_fonts[*]} =~ Inconsolata ]]; then
     echo "Installing Inconsolata font"
     curl https://github.com/google/fonts/blob/master/ofl/inconsolata/Inconsolata-Regular.ttf --output /Library/Fonts/Inconsolata-Regular.ttf
     curl https://github.com/google/fonts/blob/master/ofl/inconsolata/Inconsolata-Bold.ttf --output /Library/Fonts/Inconsolata-Bold.ttf
+    #curl https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Inconsolata/complete/Inconsolata%20Nerd%20Font%20Complete.otf --output /Library/Fonts/Inconsolata-Nerd-Font-Complete.otf
+    #curl https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Inconsolata/complete/Inconsolata%20Nerd%20Font%20Complete%20Mono.otf --output /Library/Fonts/Inconsolata-Nerd-Font-Complete-Mono.otf
+    curl https://github.com/ryanoasis/nerd-fonts/releases/download/v1.2.0/Inconsolata.zip --output ~/Downloads
 fi
