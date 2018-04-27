bc54bfa9d3dd9d9afe272aeaa56aa096872fda12
mbloodforsythe <martin.zwirnerforsythe@gmail.com>
2018-04-27 12:44:53 -0400

    Add pipenv config to fish/bash profile

--- fish/before.init.fish
+++ fish/before.init.fish
@@ -1,4 +1,5 @@
 ## Virtualfish
 # http://virtualfish.readthedocs.io/en/latest/plugins.html
-set -g VIRTUALFISH_PYTHON "/usr/local/bin/python2"
+set -g VIRTUALFISH_PYTHON "/usr/local/bin/python3"
 set -g VIRTUALFISH_PLUGINS "compat_aliases"
+eval (pipenv --completion)
