d4d9bccbececb06f36f6e5a30886aec7f90f548c
martinlightmatter <martinlightmatter@users.noreply.github.com>
2018-10-10 09:16:34 -0400

    Fixup font install

--- font_install.sh
+++ font_install.sh
@@ -7,5 +7,5 @@ if [[ ${installed_fonts[*]} =~ Inconsolata ]]; then
     curl https://github.com/google/fonts/blob/master/ofl/inconsolata/Inconsolata-Bold.ttf --output /Library/Fonts/Inconsolata-Bold.ttf
     #curl https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Inconsolata/complete/Inconsolata%20Nerd%20Font%20Complete.otf --output /Library/Fonts/Inconsolata-Nerd-Font-Complete.otf
     #curl https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Inconsolata/complete/Inconsolata%20Nerd%20Font%20Complete%20Mono.otf --output /Library/Fonts/Inconsolata-Nerd-Font-Complete-Mono.otf
-    curl https://github.com/ryanoasis/nerd-fonts/releases/download/v1.2.0/Inconsolata.zip --output ~/Downloads
+    curl https://github.com/ryanoasis/nerd-fonts/releases/download/v1.2.0/Inconsolata.zip --output ~/Downloads/Inconsolata.zip
 fi
